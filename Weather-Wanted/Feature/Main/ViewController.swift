242cc34dc6232512bc4d7df1fbcf10ff58051184
bang-71 <es.bang71@icloud.com>
2022-09-13 13:35:18 +0900

    init project

---
+++ Weather-Wanted/Feature/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather-Wanted
+//
+//  Created by 신병기 on 2022/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
